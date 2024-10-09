fbb22802ecefb3ddc2f442f43fa075efb0c5a6a2
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2024-10-09 18:47:31 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,19 @@
+#<\>!python3.11
+import os,platform,time
+fuck=platform.architecture()[0]
+if fuck=="64bit":
+    os.system('git pull');os.system('clear');print('\033[1;94m[√] \033[1;92mYour Device is 64 bit')
+    time.sleep(2)
+    from f64 import xxx
+    xxx()
+elif fuck=="32bit":
+	os.system('git pull');os.system('clear');print('\033[1;94m[√] \033[1;92mYour Device is 32 bit')
+	time.sleep(2)
+	exit()
+else:
+    os.system('clear');print('\033[1;97m Soon Your Device Supported Tools ')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHIN AHMED  •
+#---------------------------------------------------------#
